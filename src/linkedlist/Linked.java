09985bf4a469365f43fcb2a9739ee402ed4fcdd7
saumye4696 <saumye.mehrotra96@gmail.com>
2026-07-19 15:10:33 +0530

    linked list traversal, insert start, insert end

--- src/linkedlist/Linked.java
+++ src/linkedlist/Linked.java
@@ -11,17 +11,63 @@ class Node {
 }
 
 public class Linked {
+    public static void traverseList(Node head) {
+        while (head != null) {
+            System.out.println(head.data);
+            head = head.next;
+        }
+    }
+
+    public static void traverseListRecusrion(Node head) {
+        if (head == null) {
+            return;
+        }
+        System.out.println(head.data);
+
+        if (head.next != null) {
+            traverseListRecusrion(head.next);
+        }
+
+    }
+
+    public static Node insertAtFront(Node head, int x) {
+        Node n = new Node(x);
+        n.next = head;
+
+        return n;
+    }
+
+    public static Node insertAtEnd(Node head, int x) {
+        Node n = new Node(x);
+
+        if (head == null)
+            return n;
+
+        Node last = head;
+
+        while (last.next != null) {
+            last = last.next;
+        }
+
+        last.next = n;
+        return head;
+
+    }
+
     public static void main(String[] args) {
         Node head = new Node(10);
         head.next = new Node(20);
-
         head.next.next = new Node(30);
         head.next.next.next = new Node(40);
 
-        Node temp = head;
-        while (temp != null) {
-            System.out.println(temp.data);
-            temp = temp.next;
-        }
+//        Linked.traverseListRecusrion(head);
+//        head = insertAtFront(head, 1);
+        head = insertAtEnd(head, 1);
+        Linked.traverseList(head);
+//        Node temp = head;
+//        while (temp != null) {
+//            System.out.println(temp.data);
+//            temp = temp.next;
+//        }
     }
 }
